e2713888f6e9af043ed9901487e113417eff64dc
Carina <cpetravicius@gmail.com>
2020-02-02 22:56:18 -0300

    Config Spring Boot application with Consul and git2consul.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # demo-consul-price
-Demo Consul Price
+Demo example of a Spring Boot application with Consul and Git2Consul
+
+##### Start the consul and git2consul with docker-compose.
+###### In the http://localhost:8500/ui
+###### you can see the Key/Value of the file properties of the project https://github.com/CarinaPetravicius/RepoGit2Consul.git.
+###### after start the spring boot application, you can see the Consul discovered the tax-service-app.
---
+++ src/main/java/br/com/demo/api/TaxController.java
@@ -0,0 +1,29 @@
+package br.com.demo.api;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class TaxController {
+
+    @Value("${taxrate}")
+    private Double taxRate;
+
+    @Value("${fix.price}")
+    private Double fixPrice;
+
+    @RequestMapping("/tax/{price}")
+    public Double calculateTax(@PathVariable(value = "price") Double price) {
+        return price * taxRate;
+    }
+
+    @RequestMapping("/price")
+    public Double calculateFixPrice() {
+        return fixPrice * taxRate;
+    }
+
+}
